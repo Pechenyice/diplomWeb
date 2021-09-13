f7785fdbad7498329115e65f026fd1f2a29097e8
pechenyice <german.iskhakov@mail.ru>
2021-09-14 00:03:52 +0300

    added business actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,18 +3,37 @@ import Client from "../Client/Client"
 const actions = {
     types: {
         APPLY_FILTERS: 'APPLY_FILTERS',
+        REMOVE_ERROR: 'REMOVE_ERROR',
+        ADD_ERROR: 'ADD_ERROR',
         CATEGORIES_REQUEST_STARTED: 'CATEGORIES_REQUEST_STARTED',
         CATEGORIES_REQUEST_FAILED: 'CATEGORIES_REQUEST_FAILED',
         CATEGORIES_REQUEST_SUCCESSED: 'CATEGORIES_REQUEST_SUCCESSED',
         TYPES_REQUEST_STARTED: 'TYPES_REQUEST_STARTED',
         TYPES_REQUEST_FAILED: 'TYPES_REQUEST_FAILED',
-        TYPES_REQUEST_SUCCESSED: 'TYPES_REQUEST_SUCCESSED'
+        TYPES_REQUEST_SUCCESSED: 'TYPES_REQUEST_SUCCESSED',
+        BUSINESSES_REQUEST_STARTED: 'BUSINESSES_REQUEST_STARTED',
+        BUSINESSES_REQUEST_FAILED: 'BUSINESSES_REQUEST_FAILED',
+        BUSINESSES_REQUEST_SUCCESSED: 'BUSINESSES_REQUEST_SUCCESSED'
     },
 
     applyFilters: function (filters) {
         return {
             type: this.types.APPLY_FILTERS,
             filters
+        }
+    },
+
+    removeError: function(id) {
+        return {
+            type: this.types.REMOVE_ERROR,
+            id
+        }
+    },
+
+    addError: function(text) {
+        return {
+            type: this.types.ADD_ERROR,
+            text
         }
     },
 
@@ -44,9 +63,43 @@ const actions = {
                 .then((categories) => {
                     dispatch(this.fetchCategoriesSuccess(categories));
                 })
-                // .catch(() => {
-                //     dispatch(this.fetchCategoriesFail());
-                // })
+                .catch(() => {
+                    dispatch(this.fetchCategoriesFail());
+                    dispatch(this.addError('Failed load categories for businesses!'));
+                })
+        }
+    },
+
+    fetchBusinessesRequest: function () {
+        return {
+            type: this.types.BUSINESSES_REQUEST_STARTED
+        }
+    },
+
+    fetchBusinessesSuccess: function (result) {
+        return {
+            type: this.types.BUSINESSES_REQUEST_SUCCESSED,
+            result
+        }
+    },
+
+    fetchBusinessesFail: function () {
+        return {
+            type: this.types.BUSINESSES_REQUEST_FAILED
+        }
+    },
+
+    fetchBusinesses: function(offset, count, filters) {
+        return (dispatch) => {
+            dispatch(this.fetchBusinessesRequest());
+            Client.loadBusinesses(offset, count, filters)
+                .then((result) => {
+                    dispatch(this.fetchBusinessesSuccess(result));
+                })
+                .catch(() => {
+                    dispatch(this.fetchBusinessesFail());
+                    dispatch(this.addError('Failed load businesses!'));
+                })
         }
     },
 
@@ -71,15 +124,15 @@ const actions = {
 
     fetchTypes: function () {
         return (dispatch) => {
-            console.log(dispatch)
             dispatch(this.fetchTypesRequest());
             Client.loadTypes()
                 .then((types) => {
                     dispatch(this.fetchTypesSuccess(types));
                 })
-                // .catch(() => {
-                //     dispatch(this.fetchTypesFail());
-                // })
+                .catch(() => {
+                    dispatch(this.fetchTypesFail());
+                    dispatch(this.addError('Failed load types for businesses!'));
+                })
         }
     }
 }

91e9ad40f7cf0378c9d64879b2d29052f7413c1f
pechenyice <german.iskhakov@mail.ru>
2021-09-14 00:02:32 +0300

    added new api calls

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,32 +1,60 @@
 const Client = {
-    loadCategories: function () {
-        return {
-            then: function (cb) {
-                setTimeout(() => {
-                    cb([])
-                }, 1000);
-            },
-            catch: function (cb) {
-                setTimeout(() => {
-                    cb([])
-                }, 1000);
-            }
+
+    apiPath: '/api',
+
+    constructUrl: function (path) {
+        return `${this.apiPath}${path}`;
+    },
+
+    checkStatus: function (response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        } else {
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            console.log(error);
+            throw error;
         }
     },
 
-    loadTypes: function () {
-        return {
-            then: function (cb) {
-                setTimeout(() => {
-                    cb([])
-                }, 1000);
-            },
-            catch: function (cb) {
-                setTimeout(() => {
-                    cb([])
-                }, 1000);
+    loadCategories: function () {
+        return fetch(this.constructUrl('/getFiltersCategories'), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-        }
+        })
+            .then(this.checkStatus)
+            .then((response) => {
+                return response.json();
+            });
+    },
+
+    loadTypes: function () {
+        return fetch(this.constructUrl('/getFiltersTypes'), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(this.checkStatus)
+            .then((response) => {
+                return response.json();
+            });
+    },
+
+    loadBusinesses: function (offset, count, filters) {
+        return fetch(this.constructUrl(`/getBusinesses?offset=${offset}&count=${count}&f_category=${filters.category}&f_type=${filters.type}&f_pattern=${filters.pattern}`), {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(this.checkStatus)
+            .then((response) => {
+                return response.json();
+            });
     },
 }
 

b451e5f2583de38d85c468fe972d1c618c6fbd40
pechenyice <german.iskhakov@mail.ru>
2021-09-13 20:15:19 +0300

    added container for routing [NEED REFACTOR]

---
+++ src/components/container/AuthorizedRouteManager/AuthorizedRouteManager.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { connect } from "react-redux";
+// import AuthorizedRoute from "../../helpers/AuthorizedRoute/AuthorizedRoute";
+
+// const AuthorizedRouteManager = (props) => {
+
+// }
+
+// function mapStateToProps(state) {
+//     return {
+//         'isLogged':  state.user == null ? false : true
+//         'component': 
+//     };
+// }
+
+// export default AuthorizedRouteManager;

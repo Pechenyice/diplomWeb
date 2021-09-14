0e2fdcf6e1a119634e665af17c82fb85398c45f5
Pechenyice <german.iskhakov@mail.ru>
2021-09-15 01:29:23 +0300

    updated presentational component for plan card view

--- src/components/presentational/BusinessPlan/BusinessPlan.js
+++ src/components/presentational/BusinessPlan/BusinessPlan.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from './BusinessPlan.module.css';
 
-const BusinessPlan = (props) => {
-    console.log(props.match)
+const BusinessPlan = ({ plan, onInit, onNullPlan, onClear, onNeedMoreComments }) => {
+
+    useEffect(() => {
+        return () => {
+            onClear();
+        }
+    }, []);
+
+    if (plan.data === null && !plan.isChecked) onInit();
+
+    if (plan.data === null && plan.isChecked && !plan.isLoading && !plan.isFetched) onNullPlan();
+
     return (
-        <div>test</div>
+        <section>
+            {
+                plan.isLoading ?
+                    <div>LOADING</div> :
+                    plan.data ?
+                        <div>
+                            <div>
+                                {plan.data.name}
+                            </div>
+                            <div>
+                                {plan.data.category}
+                            </div>
+                            <div>
+                                {plan.data.type}
+                            </div>
+                            <div>
+                                {plan.data.description}
+                            </div>
+                            <div>
+                                {plan.data.created}
+                            </div>
+                            <div>
+                                {plan.data.likes} {plan.data.dislikes}
+                            </div>
+                            <div>
+                                income: {plan.data.income.sum} {plan.data.income.text}
+                            </div>
+                            <div>
+                                expence: {plan.data.expence.sum} {plan.data.expence.text}
+                            </div>
+                        </div> :
+                        <div>NO DATA</div>
+            }
+        </section>
     )
 }
 

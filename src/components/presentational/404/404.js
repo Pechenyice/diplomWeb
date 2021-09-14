5734bff616bb431bc65bc36c0f159c375338ef5d
pechenyice <german.iskhakov@mail.ru>
2021-09-14 22:33:33 +0300

    created no path route compoennt

---
+++ src/components/presentational/404/404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './404.module.css';
+
+const InvalidRoute = () => (
+    <div>
+        404. No such path on server
+    </div>
+);
+
+export default InvalidRoute;

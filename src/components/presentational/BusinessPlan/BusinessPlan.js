a8e7bc29c85d27844bfb8f9c9e9382c66c973734
pechenyice <german.iskhakov@mail.ru>
2021-09-14 22:33:12 +0300

    created container component for plan view page

---
+++ src/components/presentational/BusinessPlan/BusinessPlan.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './BusinessPlan.module.css';
+
+const BusinessPlan = (props) => {
+    console.log(props.match)
+    return (
+        <div>test</div>
+    )
+}
+
+export default BusinessPlan;

69a2c0e2907e7e0a4a973dfd87ca962ecaebcc2f
pechenyice <german.iskhakov@mail.ru>
2021-09-17 00:56:12 +0300

    created new types end events

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -21,7 +21,22 @@ const actions = {
         PLAN_REQUEST_FAILED: 'PLAN_REQUEST_FAILED',
         COMMENTS_REQUEST_STARTED: 'COMMENTS_REQUEST_STARTED',
         COMMENTS_REQUEST_SUCCESSED: 'COMMENTS_REQUEST_SUCCESSED',
-        COMMENTS_REQUEST_FAILED: 'COMMENTS_REQUEST_FAILED'
+        COMMENTS_REQUEST_FAILED: 'COMMENTS_REQUEST_FAILED',
+        AUTH_REQUEST_STARTED: 'AUTH_REQUEST_STARTED',
+        AUTH_REQUEST_SUCCESSED: 'AUTH_REQUEST_SUCCESSED',
+        AUTH_REQUEST_FAILED: 'AUTH_REQUEST_FAILED',
+        USER_AUTH_CHECK_REQUEST_STARTED: 'USER_AUTH_CHECK_REQUEST_STARTED',
+        USER_AUTH_CHECK_REQUEST_SUCCESSED: 'USER_AUTH_CHECK_REQUEST_SUCCESSED',
+        USER_AUTH_CHECK_REQUEST_FAILED: 'USER_AUTH_CHECK_REQUEST_FAILED',
+        OWN_PLANS_REQUEST_STARTED: 'OWN_PLANS_REQUEST_STARTED',
+        OWN_PLANS_REQUEST_SUCCESSED: 'OWN_PLANS_REQUEST_SUCCESSED',
+        OWN_PLANS_REQUEST_FAILED: 'OWN_PLANS_REQUEST_FAILED',
+        LIKED_PLANS_REQUEST_STARTED: 'LIKED_PLANS_REQUEST_STARTED',
+        LIKED_PLANS_REQUEST_SUCCESSED: 'LIKED_PLANS_REQUEST_SUCCESSED',
+        LIKED_PLANS_REQUEST_FAILED: 'LIKED_PLANS_REQUEST_FAILED',
+        DISLIKED_PLANS_REQUEST_STARTED: 'DISLIKED_PLANS_REQUEST_STARTED',
+        DISLIKED_PLANS_REQUEST_SUCCESSED: 'DISLIKED_PLANS_REQUEST_SUCCESSED',
+        DISLIKED_PLANS_REQUEST_FAILED: 'DISLIKED_PLANS_REQUEST_FAILED'
     },
 
     findPlan: function({planId, edId}) {
@@ -181,7 +196,21 @@ const actions = {
         }
     },
 
-    fetchInitialComments: function(businessId, edId, offset, count) {
+    // fetchInitialComments: function(businessId, edId, offset, count) {
+    //     return (dispatch) => {
+    //         dispatch(this.fetchCommentsRequest());
+    //         Client.loadComments(businessId, edId, offset, count)
+    //             .then((result) => {
+    //                 dispatch(this.fetchCommentsSuccess(result));
+    //             })
+    //             .catch(() => {
+    //                 dispatch(this.fetchCommentsFail());
+    //                 dispatch(this.addError('Failed load comments!'));
+    //             })
+    //     }
+    // },
+
+    fetchComments: function(businessId, edId, offset, count) {
         return (dispatch) => {
             dispatch(this.fetchCommentsRequest());
             Client.loadComments(businessId, edId, offset, count)
@@ -195,16 +224,184 @@ const actions = {
         }
     },
 
-    fetchComments: function(businessId, edId, offset, count) {
+    fetchAuthRequest: function () {
+        return {
+            type: this.types.AUTH_REQUEST_STARTED
+        }
+    },
+
+    fetchAuthSuccess: function (user) {
+        return {
+            type: this.types.AUTH_REQUEST_SUCCESSED,
+            user
+        }
+    },
+
+    fetchAuthFail: function () {
+        return {
+            type: this.types.AUTH_REQUEST_FAILED
+        }
+    },
+
+    fetchAuth: function() {
         return (dispatch) => {
-            dispatch(this.fetchCommentsRequest());
-            Client.loadComments(businessId, edId, offset, count)
-                .then((result) => {
-                    dispatch(this.fetchCommentsSuccess(result));
+            dispatch(this.fetchAuthRequest());
+            Client.loadAuthData()
+                .then((user) => {
+                    dispatch(this.fetchAuthSuccess(user));
                 })
                 .catch(() => {
-                    dispatch(this.fetchCommentsFail());
-                    dispatch(this.addError('Failed load comments!'));
+                    dispatch(this.fetchAuthFail());
+                    dispatch(this.addError('Failed load account data: something went wrong!'));
+                })
+        }
+    },
+
+    fetchOwnPlansRequest: function () {
+        return {
+            type: this.types.OWN_PLANS_REQUEST_STARTED
+        }
+    },
+
+    fetchOwnPlansSuccess: function (result, userId) {
+        return {
+            type: this.types.OWN_PLANS_REQUEST_SUCCESSED,
+            result,
+            userId
+        }
+    },
+
+    fetchOwnPlansFail: function () {
+        return {
+            type: this.types.OWN_PLANS_REQUEST_FAILED
+        }
+    },
+
+    fetchOwnPlans: function(userId) {
+        return (dispatch) => {
+            dispatch(this.fetchOwnPlansRequest());
+            Client.loadOwnPlans(userId)
+                .then((result) => {
+                    dispatch(this.fetchOwnPlansSuccess(result, userId));
+                })
+                .catch(() => {
+                    dispatch(this.fetchOwnPlansFail());
+                    dispatch(this.addError('Failed load own plans for account!'));
+                })
+        }
+    },
+
+    fetchLikedPlansRequest: function () {
+        return {
+            type: this.types.LIKED_PLANS_REQUEST_STARTED
+        }
+    },
+
+    fetchLikedPlansSuccess: function (result, userId) {
+        return {
+            type: this.types.LIKED_PLANS_REQUEST_SUCCESSED,
+            result,
+            userId
+        }
+    },
+
+    fetchLikedPlansFail: function () {
+        return {
+            type: this.types.LIKED_PLANS_REQUEST_FAILED
+        }
+    },
+
+    fetchLikedPlans: function(userId) {
+        return (dispatch) => {
+            dispatch(this.fetchLikedPlansRequest());
+            Client.loadLikedPlans(userId)
+                .then((result) => {
+                    dispatch(this.fetchLikedPlansSuccess(result, userId));
+                })
+                .catch(() => {
+                    dispatch(this.fetchLikedPlansFail());
+                    dispatch(this.addError('Failed load liked plans for account!'));
+                })
+        }
+    },
+
+    fetchDislikedPlansRequest: function () {
+        return {
+            type: this.types.DISLIKED_PLANS_REQUEST_STARTED
+        }
+    },
+
+    fetchDislikedPlansSuccess: function (result, userId) {
+        return {
+            type: this.types.DISLIKED_PLANS_REQUEST_SUCCESSED,
+            result,
+            userId
+        }
+    },
+
+    fetchDislikedPlansFail: function () {
+        return {
+            type: this.types.DISLIKED_PLANS_REQUEST_FAILED
+        }
+    },
+
+    fetchDislikedPlans: function(userId) {
+        return (dispatch) => {
+            dispatch(this.fetchDislikedPlansRequest());
+            Client.loadDislikedPlans(userId)
+                .then((result) => {
+                    dispatch(this.fetchDislikedPlansSuccess(result, userId));
+                })
+                .catch(() => {
+                    dispatch(this.fetchDislikedPlansFail());
+                    dispatch(this.addError('Failed load disliked plans for account!'));
+                })
+        }
+    },
+
+    fetchUserAuthCheckRequest: function () {
+        return {
+            type: this.types.USER_AUTH_CHECK_REQUEST_STARTED
+        }
+    },
+
+    fetchUserAuthCheckSuccess: function (user) {
+        return {
+            type: this.types.USER_AUTH_CHECK_REQUEST_SUCCESSED,
+            user
+        }
+    },
+
+    fetchUserAuthCheckFail: function () {
+        return {
+            type: this.types.USER_AUTH_CHECK_REQUEST_FAILED
+        }
+    },
+
+    fetchUserAuthCheck: function() {
+        return (dispatch) => {
+            dispatch(this.fetchUserAuthCheckRequest());
+            Client.loadUserAuthCheckData()
+                .then((user) => {
+                    dispatch(this.fetchUserAuthCheckSuccess(user));
+                })
+                .catch(() => {
+                    dispatch(this.fetchUserAuthCheckFail());
+                    dispatch(this.addError('Failed load account data token: request failed!'));
+                })
+        }
+    },
+
+    fetchEditionDataForPlanView: function(planId, edId) {
+        return (dispatch) => {
+            dispatch(this.fetchEditionDataForPlanViewRequest());
+            Client.loadEditionForPlanView(planId, edId)
+                .then((result) => {
+                    dispatch(this.fetchEditionDataForPlanViewSuccess(result));
+                })
+                .catch(() => {
+                    dispatch(this.fetchEditionDataForPlanViewFail());
+                    dispatch(this.addError('Failed load plan edition data!'));
                 })
         }
     },

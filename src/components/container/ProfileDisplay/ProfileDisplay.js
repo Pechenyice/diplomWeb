0709c06d7af6f89385e50cdba0ac8f562970c7e9
pechenyice <german.iskhakov@mail.ru>
2021-09-17 01:02:13 +0300

    created container component for profile view

---
+++ src/components/container/ProfileDisplay/ProfileDisplay.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect } from "react-redux";
+import actions from "../../../redux/actions";
+import Profile from "../../presentational/Profile/Profile";
+
+const ProfileDisplay = connect(mapStateToProps, mapDispatchToProps, mergePropsWithDispatch)(Profile);
+
+function mapStateToProps(state, ownProps) {
+    console.log('state.profilePlans', state.profilePlans)
+    return {
+        userId: ownProps.match.params.userId,
+        cachedLoadedForUser: state.profilePlans.forUser,
+        profilePlans: state.profilePlans
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatch
+    };
+}
+
+function mergePropsWithDispatch(stateProps, dispatchProps) {
+    return {
+        ...stateProps,
+        ...dispatchProps,
+        onNeedLoadOwnPlans: () => {
+            dispatchProps.dispatch(actions.fetchOwnPlans(stateProps.userId))
+        }, 
+        onNeedLoadLikedPlans: () => {
+            dispatchProps.dispatch(actions.fetchLikedPlans(stateProps.userId))
+        }, 
+        onNeedLoadDislikedPlans: () => {
+            dispatchProps.dispatch(actions.fetchDislikedPlans(stateProps.userId))
+        }
+    };
+}
+
+export default ProfileDisplay;

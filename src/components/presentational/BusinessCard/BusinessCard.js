0a885cea8960a9b4f0ee46e1674651360d7f9fe3
pechenyice <german.iskhakov@mail.ru>
2021-09-18 00:57:20 +0300

    removed logs

--- src/components/presentational/BusinessCard/BusinessCard.js
+++ src/components/presentational/BusinessCard/BusinessCard.js
@@ -3,41 +3,39 @@ import styles from './BusinessCard.module.css';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
-const BusinessCard = ({ data }) => {
-    return (
-        <Link to={`/plan/${data.id}/ed/${data.editions[0].id}`} style={{margin: '50px'}}>
+const BusinessCard = ({ data }) => (
+    <Link to={`/plan/${data.id}/ed/${data.editions[0].id}`} style={{ margin: '50px' }}>
+        <div>
             <div>
-                <div>
-                    {data.editions[0].content.name}
-                </div>
-                <div>
-                    {data.editions[0].content.category}
-                </div>
-                <div>
-                    {data.editions[0].content.type}
-                </div>
-                <div>
-                    {data.editions[0].content.description}
-                </div>
-                <div>
-                    {data.editions[0].content.created}
-                </div>
-                <div>
-                    {data.editions[0].content.likes} {data.editions[0].content.dislikes}
-                </div>
-                <div>
-                    income: {data.editions[0].content.income.sum} {data.editions[0].content.income.text}
-                </div>
-                <div>
-                    expence: {data.editions[0].content.expence.sum} {data.editions[0].content.expence.text}
-                </div>
-                {
-                    data.editions.map(e => (<div key={e.id}>{e.id}</div>))
-                }
+                {data.editions[0].content.name}
             </div>
-        </Link>
-    );
-}
+            <div>
+                {data.editions[0].content.category}
+            </div>
+            <div>
+                {data.editions[0].content.type}
+            </div>
+            <div>
+                {data.editions[0].content.description}
+            </div>
+            <div>
+                {data.editions[0].content.created}
+            </div>
+            <div>
+                {data.editions[0].content.likes} {data.editions[0].content.dislikes}
+            </div>
+            <div>
+                income: {data.editions[0].content.income.sum} {data.editions[0].content.income.text}
+            </div>
+            <div>
+                expence: {data.editions[0].content.expence.sum} {data.editions[0].content.expence.text}
+            </div>
+            {
+                data.editions.map(e => (<div key={e.id}>{e.id}</div>))
+            }
+        </div>
+    </Link>
+);
 
 BusinessCard.propTypes = {
     data: PropTypes.objectOf(PropTypes.any).isRequired

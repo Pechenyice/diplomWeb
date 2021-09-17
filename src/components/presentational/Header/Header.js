23174044d0d73c882f764c19f8a004669d835fc8
pechenyice <german.iskhakov@mail.ru>
2021-09-18 00:58:10 +0300

    miserable changes

--- src/components/presentational/Header/Header.js
+++ src/components/presentational/Header/Header.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import styles from './Header.module.css';
 
-const Header = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.headerBlock}>
-                <Logo />
-                <Link to={'/newPlan'}>Add business</Link>
-            </div>
-            <div className={styles.headerBlock}>
-                <Link to={'/catalog'}>to catalog</Link>
-                <Link to={'/profile'}>profile</Link>
-            </div>
-        </header>
-    );
-}
+const Header = () => (
+    <header className={styles.header}>
+        <div className={styles.headerBlock}>
+            <Logo />
+            <Link to={'/newPlan'}>Add business</Link>
+        </div>
+        <div className={styles.headerBlock}>
+            <Link to={'/catalog'}>to catalog</Link>
+            <Link to={'/profile'}>profile</Link>
+        </div>
+    </header>
+);
 
 export default Header;

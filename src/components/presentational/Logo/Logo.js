fbfb46a4909e239a48502d4e0bf8d5fa4b05a6a4
pechenyice <german.iskhakov@mail.ru>
2021-09-18 00:58:46 +0300

    miserable changes (= refactor () => {} to () => ())

--- src/components/presentational/Logo/Logo.js
+++ src/components/presentational/Logo/Logo.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Logo.module.css';
 
-const Logo = () => {
-    return (
-        <Link to={'/'}>
-            <div>Logo</div>
-        </Link>
-    );
-}
+const Logo = () => (
+    <Link to={'/'}>
+        <div>Logo</div>
+    </Link>
+);
 
 export default Logo;

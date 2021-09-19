3a23da18b65c2417c842531b43bc2fae7734567a
Pechenyice <german.iskhakov@mail.ru>
2021-09-19 03:04:06 +0300

    created layout for input presentational component

---
+++ src/components/presentational/Input/Input.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from './Input.module.css';
+import PropTypes from 'prop-types';
+
+const Input = ({ id, label, isEmpty, onChange }) => {
+    const classes = [styles.inputWrapper];
+    if (isEmpty) classes.push(styles.emptyInputWrapper);
+
+    const labelClasses = [styles.inputLabel];
+    if (!isEmpty) labelClasses.push(styles.notEmptyinputLabel);
+
+    console.log(labelClasses, isEmpty)
+
+    return (
+        <div className={classes.join(' ')}>
+            <input className={styles.input} id={id} onChange={onChange} />
+            <label className={labelClasses.join(' ')} htmlFor={id}>{label}</label>
+        </div>
+    );
+}
+
+Input.propTypes = {
+    id: PropTypes.string,
+    label: PropTypes.string,
+    isEmpty: PropTypes.bool,
+    onChange: PropTypes.func
+}
+
+export default Input;

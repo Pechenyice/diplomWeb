6358874523dc0fd66ea1553ada9ef1053c88cdd9
Pechenyice <german.iskhakov@mail.ru>
2021-09-19 13:42:24 +0300

    created module for helping with work with db

---
+++ server/dbUtils.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql2/promise');
+const uuid = require('uuid');
+require('dotenv').config();
+
+let connection = null;
+
+(async () => {
+    connection = await mysql.createConnection({
+        host: process.env.DBHOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: process.env.DBNAME
+    });
+})();
+
+const dbUtils = {
+    addUser: async () => {
+        let id = uuid.v4();
+        let answer = await connection.execute(
+            `INSERT INTO User (_id, login, password, photo_path, nickname)
+            SELECT * FROM (SELECT ? as _id, ? as login, ? as password, null as photo_path, ? as nickname) AS tmp
+            WHERE NOT EXISTS (
+                SELECT login FROM User WHERE login = ? or nickname = ?
+            ) LIMIT 1;`,
+            [id, 'root1', 'root', 'admin', 'root1', 'admin']
+        );
+
+        return {id, answer}
+    },
+
+    setToken: async (id, deathDate, ip) => {
+        let token = uuid.v4();
+        let answer = await connection.execute(
+            `INSERT INTO Token (body, user_id, death_date, ip) values (?, ?, ?, ?);`,
+            [token, id, deathDate, ip]
+        );
+
+        return token;
+    },
+}
+
+// const connection = await dbCon.getConnection();
+// try {
+//   await connection.query('START TRANSACTION');
+//   await query('INSERT INTO `tbl_activity_log` (dt, tm,userid,username,activity) VALUES(?,?,?,?,?)',
+//                     ['2019-02-21', '10:22:01', 'S', 'Pradip', 'RAhul3']);
+//   await dbCon.query('INSERT INTO `tbl_activity_log` (dt, tm,userid,username,activity) VALUES(?,?,?,?,?)', ['2019-02-21', '10:22:01', 'S', 'Pradip','this is test and the valid out put is this and then']);
+//   await connection.release();
+// } catch(e) {
+//   await connection.query('ROLLBACK');
+//   await connection.release();
+// }
+
+module.exports = dbUtils;

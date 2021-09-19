9e1a88f9d703acb63cb6da51ece6f569fd0b9375
Pechenyice <german.iskhakov@mail.ru>
2021-09-20 01:08:20 +0300

    added logic to auth middleware

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,3 +1,5 @@
+const dbUtils = require('./dbUtils');
+
 const middlewares = {
     bindLogs: function(req, res, next) {
         console.log(`User action:\nip: ${req.ip}\npath: ${req.originalUrl}`)
@@ -10,7 +12,29 @@ const middlewares = {
             return;
         }
 
-        next();
+        (async () => {
+            let [result, fields] = await dbUtils.getUserByToken(req.cookies.authToken);
+            
+            console.log(result)
+
+            if (!result.length) {
+                res.send(JSON.stringify({AUTH: 'FAIL'}));
+                return;
+            }
+
+            if (result[0].ip != req.ip) {
+                res.send(JSON.stringify({AUTH: 'FAIL'}));
+                return;
+            }
+
+            if (result[0].death_date < Date.now()) {
+                res.send(JSON.stringify({AUTH: 'FAIL'}));
+                dbUtils.dropToken(result[0].body);
+                return;
+            }
+
+            next();
+        })();
     }
 };
 

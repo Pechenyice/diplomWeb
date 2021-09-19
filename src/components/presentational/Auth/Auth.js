69788f30895e86a55962918d3e3ce0caeadf13a4
Pechenyice <german.iskhakov@mail.ru>
2021-09-19 03:04:59 +0300

    started layout for Auth presentational component

--- src/components/presentational/Auth/Auth.js
+++ src/components/presentational/Auth/Auth.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router";
 import styles from './Auth.module.css';
 import PropTypes from 'prop-types';
 import Client from "../../../Client/Client";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+import { Link } from "react-router-dom";
 
 const Auth = ({ location, isLogged, onAuthTry }) => {
     useEffect(() => {
@@ -10,6 +13,20 @@ const Auth = ({ location, isLogged, onAuthTry }) => {
             Client.abortLoadAuthDataFetch();
         }
     }, []);
+
+    const [state, setState] = useState({
+        signIn: {
+            login: '',
+            pass: ''
+        },
+        signUp: {
+            login: '',
+            nick: '',
+            pass: '',
+            rePass: '',
+            agreement: false
+        }
+    });
 
     const redirectPath = () => {
         const locationState = location.state;
@@ -19,16 +36,47 @@ const Auth = ({ location, isLogged, onAuthTry }) => {
         return pathname || '/profile';
     };
 
+    function handleSignInLoginChange(e) {
+        setState(
+            Object.assign(
+                {},
+                state,
+                {
+                    signIn: Object.assign({}, state.signIn, { login: e.target.value })
+                }
+            )
+        );
+    }
+
     return (
-        <div>
+        <section className={styles.authWrapper}>
             {
                 isLogged ?
-                <Redirect to={redirectPath()} /> : 
-                null
+                    <Redirect to={redirectPath()} /> :
+                    null
             }
-            <div>Хто я</div>
-            <div onClick={() => {onAuthTry()}}>Авторизоваться</div>
-        </div>
+            <h1><span className={styles.authSignIn}>SIGN IN</span> <span className={styles.authSignUp}>& CREATE ACCOUNT</span></h1>
+            <p className={styles.authHint}>We will redirect you to your target just in<br />a moment, introduce yourself please</p>
+            <div className={styles.inputsBlock} >
+                <div className={styles.inputsWrapper} >
+                    <Input id={'signInlogin'} label={'Login/Email'} isEmpty={!state.signIn.login} onChange={handleSignInLoginChange} />
+                    <Input id={'signInPassword'} label={'Password'} isEmpty={!state.signIn.pass} onChange={() => {}} />
+                    <Button text={'Sign in'} onClick={() => {}} style={{marginTop: '15px'}} />
+                </div>
+                <div className={styles.inputsWrapper} >
+                    <Input id={'signUplogin'} label={'Login/Email'} isEmpty={!state.signUp.login} onChange={() => {}} />
+                    <Input id={'signUpNickname'} label={'Nickname'} isEmpty={!state.signUp.nick} onChange={() => {}} />
+                    <Input id={'signUpPassword'} label={'Password'} isEmpty={!state.signUp.pass} onChange={() => {}} />
+                    <Input id={'signUpRePassword'} label={'Repeat password'} isEmpty={!state.signUp.rePass} onChange={() => {}} />
+                    <div style={{margin: '15px 0'}}>
+                        <input type='checkbox' id='checkbox'/>
+                        <label style={{marginLeft: '10px'}} htmlFor='checkbox'>I agree <Link to={'/terms'}>Terms & conditions</Link></label>
+                    </div>
+                    <Button text={'Create account'} onClick={() => {}} />
+                </div>
+            </div>
+            {/* <div onClick={() => { onAuthTry() }}>Авторизоваться</div> */}
+        </section>
     );
 }
 

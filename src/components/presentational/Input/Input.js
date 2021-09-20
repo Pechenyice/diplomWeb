f5d9cbe2c121f0bc3707a801f847ecdf5691bdff
pechenyice <german.iskhakov@mail.ru>
2021-09-21 02:17:47 +0300

    added error handler

--- src/components/presentational/Input/Input.js
+++ src/components/presentational/Input/Input.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from './Input.module.css';
 import PropTypes from 'prop-types';
 
-const Input = ({ id, label, isEmpty, onChange }) => {
+const Input = ({ id, label, isEmpty, value, error='', onChange }) => {
     const classes = [styles.inputWrapper];
     if (isEmpty) classes.push(styles.emptyInputWrapper);
 
@@ -11,8 +11,9 @@ const Input = ({ id, label, isEmpty, onChange }) => {
 
     return (
         <div className={classes.join(' ')}>
-            <input className={styles.input} id={id} onChange={onChange} />
+            <input className={styles.input} id={id} value={value} onChange={onChange} />
             <label className={labelClasses.join(' ')} htmlFor={id}>{label}</label>
+            <p className={styles.error}>{error}</p>
         </div>
     );
 }
@@ -21,6 +22,8 @@ Input.propTypes = {
     id: PropTypes.string,
     label: PropTypes.string,
     isEmpty: PropTypes.bool,
+    value: PropTypes.string,
+    error: PropTypes.string,
     onChange: PropTypes.func
 }
 

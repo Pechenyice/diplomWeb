01f636788e1083bdbd6f51ea3cea7fa521e8b039
pechenyice <german.iskhakov@mail.ru>
2021-09-21 22:02:03 +0300

    refactor to new design

--- src/components/presentational/Profile/Profile.js
+++ src/components/presentational/Profile/Profile.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import Client from "../../../Client/Client";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
-import { NavLink, Redirect, Switch, Route } from "react-router-dom";
+import { NavLink, Redirect, Switch, Route, Link } from "react-router-dom";
 import InvalidRoute from "../404/404";
 import BusinessCard from "../BusinessCard/BusinessCard";
 import Typed from "typed.js";
@@ -21,6 +21,11 @@ const Profile = ({
 	onNeedLoadOwnPlans,
 	onNeedLoadLikedPlans,
 	onNeedLoadDislikedPlans,
+	onSaveProfileData,
+	onSaveProfilePassword,
+	onLogout,
+	onError,
+	onClear,
 	location,
 }) => {
 	const el = useRef(null);
@@ -46,24 +51,23 @@ const Profile = ({
 		)
 			onNeedLoadDislikedPlans();
 
-		if (nickname == null) onNeedUserNickname();
+		if (nickname === null) onNeedUserNickname();
 
 		const options = {
 			strings: [`Welcome home, ${nickname} `],
 			typeSpeed: 50,
 		};
 
-		typed.current = new Typed(el.current, options);
+		if (nickname !== null) typed.current = new Typed(el.current, options);
 
 		return () => {
-			typed.current.destroy();
+			typed.current?.destroy();
+			// onClear();
 			// Client.abortLoadOwnPlansFetch();
 			// Client.abortLoadLikedPlansFetch();
 			// Client.abortLoadDislikedPlansFetch();
 		};
-	}, []);
-
-	console.log(login, nickname)
+	}, [nickname]);
 
 	const [state, setState] = useState({
 		data: {
@@ -71,19 +75,19 @@ const Profile = ({
 			nick: nickname || "",
 		},
 		pass: {
-			oldPass: '',
+			oldPass: "",
 			pass: "",
 			rePass: "",
 		},
 	});
 
-	function handleDataLoginChange(e) {
-		setState(
-			Object.assign({}, state, {
-				data: Object.assign({}, state.data, { login: e.target.value }),
-			})
-		);
-	}
+	// function handleDataLoginChange(e) {
+	// 	setState(
+	// 		Object.assign({}, state, {
+	// 			data: Object.assign({}, state.data, { login: e.target.value }),
+	// 		})
+	// 	);
+	// }
 
 	function handleDataNicknameChange(e) {
 		setState(
@@ -92,11 +96,13 @@ const Profile = ({
 			})
 		);
 	}
-	
+
 	function handleOldPassPasswordChange(e) {
 		setState(
 			Object.assign({}, state, {
-				pass: Object.assign({}, state.pass, { oldPass: e.target.value }),
+				pass: Object.assign({}, state.pass, {
+					oldPass: e.target.value,
+				}),
 			})
 		);
 	}
@@ -117,194 +123,307 @@ const Profile = ({
 		);
 	}
 
+	function validate(action, value) {
+		switch (action) {
+			case "profileData": {
+				if (!value) {
+					onError("Cannot save empty nickname!");
+					return false;
+				}
+				if (nickname === value) {
+					onError("Cannot save the same nickname!");
+					return false;
+				}
+				if (value.length >= 32) {
+					onError(
+						"Cannot save such a long nickname, need < 32 symbols!"
+					);
+					return false;
+				}
+				return true;
+			}
+
+			case "profilePassword": {
+			}
+		}
+	}
+
+	function handleSaveProfileData() {
+		if (validate('profileData', state.data.nick)) {
+			onSaveProfileData(state.data.nick);
+		}
+	}
+
 	return (
-		<section className={styles.profileWrapper}>
-			{/* {login}
+		<section
+			className={[
+				styles.profileWrapperDark,
+				styles.profileWrapperMain,
+			].join(" ")}
+		>
+			<section className={styles.profileWrapperLight}>
+				<section className={styles.profileWrapper}>
+					{/* {login}
 			{!userDataIsLoading ? (nickname ? nickname : "NO DATA") : "LOADING"} */}
-			{location.pathname.split("/profile")[1].includes(businessman) ? (
-				<Redirect to={"/profile/own"} />
-			) : null}
-			{location.pathname.split("/profile")[1] === "" ||
-			location.pathname.split("/profile")[1] === "/" ? (
-				<Redirect
-					to={`${location.pathname}/own`.replace(/\/\//, "/")}
-				/>
-			) : null}
-			<h1>PROFILE</h1>
-			<div style={{display: "flex"}}><p className={styles.typed} ref={el}></p></div>
-			<div className={styles.profileDataWrapper}>
-				<div className={styles.profileDummy}></div>
-				<div className={styles.inputsWrapper}>
-					<h2 className={styles.subTitle}>Edit profile</h2>
-					<Input
-						id={"datalogin"}
-						label={"Login/Email"}
-						isEmpty={!state.data.login}
-						value={state.data.login}
-						onChange={handleDataLoginChange}
-					/>
-					<Input
-						id={"dataNickname"}
-						label={"Nickname"}
-						isEmpty={!state.data.nick}
-						value={state.data.nick}
-						onChange={handleDataNicknameChange}
-					/>
-					<Button
-						text={"Apply changes"}
-						onClick={() => {}}
-						style={{ marginTop: "15px" }}
-					/>
-				</div>
-				<div className={styles.inputsWrapper}>
-				<h2 className={styles.subTitle}>Change password</h2>
-				<Input
-						id={"oldPassPass"}
-						label={"Old password"}
-						isEmpty={!state.pass.oldPass}
-						onChange={handleOldPassPasswordChange}
-					/>
-					<Input
-						id={"passPass"}
-						label={"New password"}
-						isEmpty={!state.pass.pass}
-						onChange={handlePassPasswordChange}
-					/>
-					<Input
-						id={"passRePass"}
-						label={"Repeat password"}
-						isEmpty={!state.pass.rePass}
-						onChange={handlePassRePasswordChange}
-					/>
-					<Button
-						text={"Change password"}
-						onClick={() => {}}
-						style={{ marginTop: "15px" }}
-					/>
-				</div>
-			</div>
-			<div className={styles.plansPartitionsWrapper}>
-				<NavLink
-					to={`${
-						location.pathname
-							.split("/own")[0]
-							.split("/liked")[0]
-							.split("/disliked")[0]
-					}/own`.replace(/\/\//, "/")}
-					className={styles.profileLink}
-					activeClassName={styles.activeProfileLink}
-				>
-					Own plans &nbsp;
-					<span className={styles.profileLinkCount}>
-						{" "}
-						({profilePlans?.own?.content?.length || 0})
-					</span>
-				</NavLink>
-				<NavLink
-					to={`${
-						location.pathname
-							.split("/own")[0]
-							.split("/liked")[0]
-							.split("/disliked")[0]
-					}/liked`.replace(/\/\//, "/")}
-					className={styles.profileLink}
-					activeClassName={styles.activeProfileLink}
-				>
-					Favorites &nbsp;
-					<span className={styles.profileLinkCount}>
-						{" "}
-						({profilePlans?.liked?.content?.length || 0})
-					</span>
-				</NavLink>
-				<NavLink
-					to={`${
-						location.pathname
-							.split("/own")[0]
-							.split("/liked")[0]
-							.split("/disliked")[0]
-					}/disliked`.replace(/\/\//, "/")}
-					className={styles.profileLink}
-					activeClassName={styles.activeProfileLink}
-				>
-					Trash &nbsp;
-					<span className={styles.profileLinkCount}>
-						{" "}
-						({profilePlans?.disliked?.content?.length || 0})
-					</span>
-				</NavLink>
-			</div>
-			<Switch>
-				<Route
-					path={
-						`${location.pathname}`.split("/profile")[0] +
-						`/profile${
-							`${location.pathname}`
-								.split("/profile")[1]
-								.split("/own")[0]
-								.split("/liked")[0]
-								.split("/disliked")[0]
-						}/own`
-					}
-					render={() => (
-						<div className={styles.plansWrapper}>
-							{profilePlans.own.isLoading
-								? "LOADING OWN PLANS"
-								: profilePlans.own.content.length
-								? profilePlans.own.content.map((e) => (
-										<BusinessCard data={e} key={e.id} />
-								  ))
-								: "NO OWN DATA"}
+					{location.pathname
+						.split("/profile")[1]
+						.includes(businessman) ? (
+						<Redirect to={"/profile/own"} />
+					) : null}
+					{location.pathname.split("/profile")[1] === "" ||
+					location.pathname.split("/profile")[1] === "/" ? (
+						<Redirect
+							to={`${location.pathname}/own`.replace(/\/\//, "/")}
+						/>
+					) : null}
+
+					<div className={styles.profileDataWrapperTop}>
+						<h1 className={styles.profileMainText}>PROFILE</h1>
+						<div className={styles.profileDataInfo}>
+							<div className={styles.profileDummy}></div>
+							<div style={{ display: "flex" }}>
+								<p className={styles.typed} ref={el}></p>
+							</div>
 						</div>
-					)}
-				/>
-				<Route
-					path={
-						`${location.pathname}`.split("/profile")[0] +
-						`/profile${
-							`${location.pathname}`
-								.split("/profile")[1]
-								.split("/own")[0]
-								.split("/liked")[0]
-								.split("/disliked")[0]
-						}/liked`
-					}
-					render={() => (
-						<div className={styles.plansWrapper}>
-							{profilePlans.liked.isLoading
-								? "LOADING LIKED PLANS"
-								: profilePlans.liked.content.length
-								? profilePlans.liked.content.map((e) => (
-										<BusinessCard data={e} key={e.id} />
-								  ))
-								: "NO LIKED DATA"}
+					</div>
+					<div className={styles.profileDataWrapper}>
+						<div className={styles.inputsWrapper}>
+							<h2 className={styles.subTitle}>Edit profile</h2>
+							<Input
+								id={"datalogin"}
+								label={"Login/Email"}
+								isEmpty={!state.data.login}
+								value={state.data.login}
+								onChange={() => {}}
+								readonly
+							/>
+							<Input
+								id={"dataNickname"}
+								label={"Nickname"}
+								isEmpty={!state.data.nick}
+								value={state.data.nick}
+								onChange={handleDataNicknameChange}
+							/>
+							<Button
+								text={"Apply changes"}
+								onClick={handleSaveProfileData}
+								style={{ marginTop: "15px" }}
+							/>
 						</div>
-					)}
-				/>
-				<Route
-					path={
-						`${location.pathname}`.split("/profile")[0] +
-						`/profile${
-							`${location.pathname}`
-								.split("/profile")[1]
-								.split("/own")[0]
-								.split("/liked")[0]
-								.split("/disliked")[0]
-						}/disliked`
-					}
-					render={() => (
-						<div className={styles.plansWrapper}>
-							{profilePlans.disliked.isLoading
-								? "LOADING DISLIKED PLANS"
-								: profilePlans.disliked.content.length
-								? profilePlans.disliked.content.map((e) => (
-										<BusinessCard data={e} key={e.id} />
-								  ))
-								: "NO DISLIKED DATA"}
+						<div className={styles.inputsWrapper}>
+							<h2 className={styles.subTitle}>Change password</h2>
+							<Input
+								id={"oldPassPass"}
+								label={"Old password"}
+								isEmpty={!state.pass.oldPass}
+								onChange={handleOldPassPasswordChange}
+								password
+							/>
+							<Input
+								id={"passPass"}
+								label={"New password"}
+								isEmpty={!state.pass.pass}
+								onChange={handlePassPasswordChange}
+								password
+							/>
+							<Input
+								id={"passRePass"}
+								label={"Repeat password"}
+								isEmpty={!state.pass.rePass}
+								onChange={handlePassRePasswordChange}
+								password
+							/>
+							<Button
+								text={"Change password"}
+								onClick={() => {}}
+								style={{ marginTop: "15px" }}
+							/>
 						</div>
-					)}
-				/>
-				<Route component={InvalidRoute} />
-			</Switch>
+						<div className={styles.inputsWrapper}>
+							<h2 className={styles.subTitle}>Manage profile</h2>
+							<Button text={"Logout"} onClick={onLogout} />
+						</div>
+					</div>
+				</section>
+			</section>
+			<section className={styles.profileWrapperDark}>
+				<section
+					className={[
+						styles.profileWrapper,
+						styles.profileWrapperLast,
+					].join(" ")}
+				>
+					<div className={styles.plansMainWrapper}>
+						<div className={styles.plansPartitionsWrapper}>
+							<NavLink
+								to={`${
+									location.pathname
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/own`.replace(/\/\//, "/")}
+								className={styles.profileLink}
+								activeClassName={styles.activeProfileLink}
+							>
+								Own plans &nbsp;
+								<span className={styles.profileLinkCount}>
+									{" "}
+									({profilePlans?.own?.content?.length || 0})
+								</span>
+							</NavLink>
+							<NavLink
+								to={`${
+									location.pathname
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/liked`.replace(/\/\//, "/")}
+								className={styles.profileLink}
+								activeClassName={styles.activeProfileLink}
+							>
+								Favorites &nbsp;
+								<span className={styles.profileLinkCount}>
+									{" "}
+									({profilePlans?.liked?.content?.length || 0}
+									)
+								</span>
+							</NavLink>
+							<NavLink
+								to={`${
+									location.pathname
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/disliked`.replace(/\/\//, "/")}
+								className={styles.profileLink}
+								activeClassName={styles.activeProfileLink}
+							>
+								Trash &nbsp;
+								<span className={styles.profileLinkCount}>
+									{" "}
+									(
+									{profilePlans?.disliked?.content?.length ||
+										0}
+									)
+								</span>
+							</NavLink>
+						</div>
+						<Link
+							to={"/newPlan"}
+							className={styles.plansCreateButton}
+						>
+							<Button
+								theme={"dark"}
+								text={"Create a business plan"}
+							/>
+						</Link>
+					</div>
+					<Switch>
+						<Route
+							path={
+								`${location.pathname}`.split("/profile")[0] +
+								`/profile${
+									`${location.pathname}`
+										.split("/profile")[1]
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/own`
+							}
+							render={() => (
+								<div className={styles.plansWrapper}>
+									{profilePlans.own.isLoading ? (
+										<p className={styles.plansHint}>
+											Loading own plans...
+										</p>
+									) : profilePlans.own.content.length ? (
+										profilePlans.own.content.map((e) => (
+											<BusinessCard
+												theme={"dark"}
+												data={e}
+												key={e.id}
+											/>
+										))
+									) : (
+										<p className={styles.plansHint}>
+											You have no own plans :(
+										</p>
+									)}
+								</div>
+							)}
+						/>
+						<Route
+							path={
+								`${location.pathname}`.split("/profile")[0] +
+								`/profile${
+									`${location.pathname}`
+										.split("/profile")[1]
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/liked`
+							}
+							render={() => (
+								<div className={styles.plansWrapper}>
+									{profilePlans.liked.isLoading ? (
+										<p className={styles.plansHint}>
+											Loading liked plans...
+										</p>
+									) : profilePlans.liked.content.length ? (
+										profilePlans.liked.content.map((e) => (
+											<BusinessCard
+												theme={"dark"}
+												data={e}
+												key={e.id}
+											/>
+										))
+									) : (
+										<p className={styles.plansHint}>
+											You have no liked plans :(
+										</p>
+									)}
+								</div>
+							)}
+						/>
+						<Route
+							path={
+								`${location.pathname}`.split("/profile")[0] +
+								`/profile${
+									`${location.pathname}`
+										.split("/profile")[1]
+										.split("/own")[0]
+										.split("/liked")[0]
+										.split("/disliked")[0]
+								}/disliked`
+							}
+							render={() => (
+								<div className={styles.plansWrapper}>
+									{profilePlans.disliked.isLoading ? (
+										<p className={styles.plansHint}>
+											Loading diliked plans...
+										</p>
+									) : profilePlans.disliked.content.length ? (
+										profilePlans.disliked.content.map(
+											(e) => (
+												<BusinessCard
+													theme={"dark"}
+													data={e}
+													key={e.id}
+												/>
+											)
+										)
+									) : (
+										<p className={styles.plansHint}>
+											You have no disliked plans :(
+										</p>
+									)}
+								</div>
+							)}
+						/>
+						<Route component={InvalidRoute} />
+					</Switch>
+				</section>
+			</section>
 		</section>
 	);
 };
@@ -320,6 +439,11 @@ Profile.propTypes = {
 	onNeedLoadLikedPlans: PropTypes.func,
 	onNeedLoadDislikedPlans: PropTypes.func,
 	onNeedUserNickname: PropTypes.func,
+	onSaveProfileData: PropTypes.func,
+	onSaveProfilePassword: PropTypes.func,
+	onLogout: PropTypes.func,
+	onError: PropTypes.func,
+	onClear: PropTypes.func,
 	location: PropTypes.object,
 };
 

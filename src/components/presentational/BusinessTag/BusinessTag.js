43895416abf05ea2a58e6392b1b32252f933a85b
pechenyice <german.iskhakov@mail.ru>
2021-09-21 22:00:18 +0300

    added dark theme layout and styles

--- src/components/presentational/BusinessTag/BusinessTag.js
+++ src/components/presentational/BusinessTag/BusinessTag.js
@@ -2,12 +2,13 @@ import React from "react";
 import styles from './BusinessTag.module.css';
 import PropTypes from 'prop-types';
 
-const BusinessTag = ({ text }) => (
-    <div className={styles.tag} >{text}</div>
+const BusinessTag = ({ text, theme }) => (
+    <div className={theme === 'dark' ? styles.darkThemeTag : styles.lightThemetag} >{text}</div>
 );
 
 BusinessTag.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    theme: PropTypes.string
 }
 
 export default BusinessTag;

e1c075d5e4b0dd3544d10102392ae2b5761f9498
Pechenyice <german.iskhakov@mail.ru>
2021-09-26 01:25:20 +0300

    created comment component

---
+++ src/components/presentational/BusinessPlan/Comment.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from './BusinessPlan.module.css';
+import PropTypes from 'prop-types';
+
+const Comment = ({ author, text, created, style }) => {
+    function getHumanizedMonth(n) {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+    
+        return monthNames[n]
+    }
+    
+    function humanizeDate(ts) {
+        var dt = new Date(+ts);
+    
+        return `${getHumanizedMonth(dt.getMonth())} ${dt.getDate()}, ${dt.getFullYear()} ${dt.getHours()}:${dt.getMinutes()}`;
+    }
+
+    return (
+        <div style={style} className={styles.commentElem}>
+            <div className={styles.commentHeader}>
+                <p className={styles.commentAuthor}>{author.nickname}</p>
+                <p className={styles.commentDate}>{humanizeDate(created)}</p>
+            </div>
+            <p className={styles.commentText}>{text}</p>
+        </div>
+    );
+}
+
+Comment.propTypes = {
+    author: PropTypes.string,
+    text: PropTypes.string,
+    created: PropTypes.number,
+    style: PropTypes.object
+}
+
+export default Comment;

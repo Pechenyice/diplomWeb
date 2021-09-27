05251b833de376c4ea02b9e70383d681a315b603
Pechenyice <german.iskhakov@mail.ru>
2021-09-27 03:49:53 +0300

    added comment component

--- src/components/presentational/BusinessPlan/Comment.js
+++ src/components/presentational/BusinessPlan/Comment.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './BusinessPlan.module.css';
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 const Comment = ({ author, text, created, style }) => {
     function getHumanizedMonth(n) {
@@ -14,13 +15,13 @@ const Comment = ({ author, text, created, style }) => {
     function humanizeDate(ts) {
         var dt = new Date(+ts);
     
-        return `${getHumanizedMonth(dt.getMonth())} ${dt.getDate()}, ${dt.getFullYear()} ${dt.getHours()}:${dt.getMinutes()}`;
+        return `${getHumanizedMonth(dt.getMonth())} ${dt.getDate()}, ${dt.getFullYear()} ${dt.getHours()}:${('0' + dt.getMinutes()).slice(-2)}`;
     }
 
     return (
         <div style={style} className={styles.commentElem}>
             <div className={styles.commentHeader}>
-                <p className={styles.commentAuthor}>{author.nickname}</p>
+                <Link to={`/profile/${author.id}/own`} ><p className={styles.commentAuthor}>{author.nickname}</p></Link>
                 <p className={styles.commentDate}>{humanizeDate(created)}</p>
             </div>
             <p className={styles.commentText}>{text}</p>

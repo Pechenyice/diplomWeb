e396b3a2518fd6a5fa9d6a28cebdb90b440656fd
Pechenyice <german.iskhakov@mail.ru>
2021-09-29 22:16:57 +0300

    added big size prop

--- src/components/presentational/Logo/Logo.js
+++ src/components/presentational/Logo/Logo.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Logo.module.css';
+import PropTypes from 'prop-types';
 
-const Logo = () => (
-    <Link to={'/'}>
-        <div className={styles.linkWrapper}><span>Businass</span><span className={styles.linkHint}>area<br/><span className={styles.linkHintHelper}>for</span><br/>idea</span></div>
-    </Link>
+const Logo = ({ big = false }) => (
+    <>
+        {
+            big ?
+                <div className={[styles.linkWrapper, big && styles.bigLinkWrapper].join(' ')}><span>Businass</span></div> :
+                <Link to={'/'}>
+                    <div className={styles.linkWrapper}><span>Businass</span><span className={styles.linkHint}>area<br /><span className={styles.linkHintHelper}>for</span><br />idea</span></div>
+                </Link>
+        }
+    </>
 );
+
+Logo.propTypes = {
+    big: PropTypes.bool
+}
 
 export default Logo;

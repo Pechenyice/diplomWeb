4c0266b886224dea83a8950425fa0e6ef5c6b286
pechenyice <german.iskhakov@mail.ru>
2021-10-05 11:30:13 +0300

    added locker svg

--- src/svgs/svgs.js
+++ src/svgs/svgs.js
@@ -1347,7 +1347,7 @@ const svgs = {
 		</svg>
 	),
 
-	'landingMainDecoration': (
+	landingMainDecoration: (
 		<svg
 			width="848"
 			height="253"
@@ -1385,6 +1385,33 @@ const svgs = {
 					/>
 				</clipPath>
 			</defs>
+		</svg>
+	),
+
+	lockerSvg: (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="50"
+			height="50"
+			viewBox="0 0 100 100"
+			preserveAspectRatio="xMidYMid"
+		>
+			<path
+				fill="none"
+				stroke="#ff708b"
+				stroke-width="10"
+				stroke-dasharray="177.0463604736328 79.54256774902345"
+				d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z"
+				stroke-linecap="round"
+			>
+				<animate
+					attributeName="stroke-dashoffset"
+					repeatCount="indefinite"
+					dur="0.9174311926605504s"
+					keyTimes="0;1"
+					values="0;256.58892822265625"
+				></animate>
+			</path>
 		</svg>
 	),
 };

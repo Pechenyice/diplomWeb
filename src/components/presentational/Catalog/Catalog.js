2446c93c549a4dce9d9517cba157bc76160ce25f
Pechenyice <german.iskhakov@mail.ru>
2021-10-09 20:08:20 +0300

    fixed duplicate plans bug

--- src/components/presentational/Catalog/Catalog.js
+++ src/components/presentational/Catalog/Catalog.js
@@ -32,15 +32,18 @@ const Catalog = ({
 			(!types.content.length && !types.isLoading)
 		)
 			onInit();
-		if (businesses.content.length < businesses.count)
-			onNeedMoreBusinesses();
+		
 		setState(filters);
 	}, [filters]);
 
+	useEffect(() => {
+		if (businesses.content.length < businesses.count && 
+			onNeedMoreBusinesses // delete this if businesses loading failes
+			)
+			onNeedMoreBusinesses();
+	}, []);
+
 	function handleFiltersSubmitClick() {
-		// if (JSON.stringify(filters) === JSON.stringify(state)) {
-		//     return;
-		// }
 		setState(Object.assign({}, state));
 		onFiltersSelected(state);
 	}

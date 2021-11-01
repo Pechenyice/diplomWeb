6b1ce8398c59b041ce7213e64c6a5d1fad2fd64b
Pechenyice <german.iskhakov@mail.ru>
2021-11-02 01:58:09 +0300

    updated initial script

--- cypress/integration/initial.spec.js
+++ cypress/integration/initial.spec.js
@@ -1,7 +1,10 @@
-// const cy = require('cypress');
-
 describe('App loads right', () => {
   it('show landing content', () => {
     cy.visit('http://localhost:3000');
+    cy.xpath('/html/body/div/section/section[1]/div[1]/h1')
+      .should(
+        "have.text",
+        "Business area for business idea"
+      );
   });
 });

f323088aab15ef3578def443e6446954306492b6
Pechenyice <german.iskhakov@mail.ru>
2021-11-03 03:31:02 +0300

    added ui errors beutiful handler (error boundary)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from "react-router-dom";
-import {Provider} from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './redux/reducers';
+import CommonErrorBoundary from './components/helpers/ErrorBounderies/CommonErrorBoundary';
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <Router>
+  <Provider store={store}>
+    <Router>
+      <CommonErrorBoundary>
         <App />
-      </Router>
-    </Provider>,
+      </CommonErrorBoundary>
+    </Router>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById('root')
 );

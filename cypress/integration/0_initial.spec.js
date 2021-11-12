fb3c690495da9b5b233de6a6fbadf2ceaa972f37
Pechenyice <german.iskhakov@mail.ru>
2021-11-13 01:23:15 +0300

    added comments

--- cypress/integration/0_initial.spec.js
+++ cypress/integration/0_initial.spec.js
@@ -1,12 +1,19 @@
 describe('App loads right', () => {
   it('show landing content', () => {
     cy.visit('http://localhost:3000');
+
+    /*
+     * check page renders without errors
+     */
     cy.xpath('/html/body/div/section/section[1]/div[1]/h1')
       .should(
         "have.text",
         "Business area for business idea"
       );
 
+    /*
+     * go to 1_auth spec
+     */
     cy.xpath('/html/body/div/section/header/div[2]/a[3]').click();
     cy.url().should('eq', 'http://localhost:3000/auth');
   });

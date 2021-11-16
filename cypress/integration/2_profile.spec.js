d05e51b4c79783634613e7a68b3662b7c6fee488
Pechenyice <german.iskhakov@mail.ru>
2021-11-17 02:14:41 +0300

    ended profile page test

--- cypress/integration/2_profile.spec.js
+++ cypress/integration/2_profile.spec.js
@@ -26,5 +26,74 @@ describe('Profile page is functioning right', () => {
     cy.get('input').should('have.length', 5);
   });
 
-  // it('shows page content', () => {});
+  it('validates empty fields', () => {
+    cy.get('input').should('have.length', 5);
+    cy.get('input').eq(1).clear();
+    cy.get('button').eq(0).click();
+
+    cy.get('div[class*="EventsSection_error"]')
+      .eq(0)
+      .should('have.text', 'Cannot save empty nickname!');
+  });
+
+  it('updates nickname', () => {
+    cy.get('input').should('have.length', 5);
+    cy.get('input').eq(1).clear();
+    cy.get('input').eq(1).type(`tester${generateHash()}`);
+    cy.get('button').eq(0).click();
+
+    cy.get('div[class*="EventsSection_success"]').should('have.length', 1);
+    cy.get('div[class*="EventsSection_success"]')
+      .eq(0)
+      .should('have.text', 'Nickname changed succesfully!');
+  });
+
+  it('validates wrong new password', () => {
+    cy.get('input').should('have.length', 5);
+    cy.get('input').eq(2).type(`test`);
+    cy.get('input').eq(3).type(`neverWillBeSuchPassword123`);
+    cy.get('input').eq(4).type(`neverWillBeSuchPassword`);
+    cy.get('button').eq(1).click();
+
+    cy.get('div[class*="EventsSection_error"]')
+      .eq(0)
+      .should('have.text', 'New passwords are not equal!');
+  });
+
+  it('validates wrong current password', () => {
+    cy.get('input').should('have.length', 5);
+    cy.get('input').eq(2).type(`neverWillBeSuchPassword`);
+    cy.get('input').eq(3).type(`neverWillBeSuchPassword123`);
+    cy.get('input').eq(4).type(`neverWillBeSuchPassword123`);
+    cy.get('button').eq(1).click();
+
+    cy.get('div[class*="EventsSection_error"]')
+      .eq(0)
+      .should('have.text', 'Cannot update password!');
+  });
+
+  it('updates password', () => {
+    cy.get('input').should('have.length', 5);
+    cy.get('input').eq(2).type(`test`);
+    cy.get('input').eq(3).type(`test123`);
+    cy.get('input').eq(4).type(`test123`);
+    cy.get('button').eq(1).click();
+
+    cy.get('div[class*="EventsSection_success"]')
+      .eq(0)
+      .should('have.text', 'Password changed succesfully!');
+
+    cy.get('input').eq(2).clear();
+    cy.get('input').eq(3).clear();
+    cy.get('input').eq(4).clear();
+    cy.get('input').eq(2).type(`test123`);
+    cy.get('input').eq(3).type(`test`);
+    cy.get('input').eq(4).type(`test`);
+    cy.get('button').eq(1).click();
+
+    cy.get('div[class*="EventsSection_success"]').should('have.length', 2);
+    cy.get('div[class*="EventsSection_success"]')
+      .eq(1)
+      .should('have.text', 'Password changed succesfully!');
+  });
 });

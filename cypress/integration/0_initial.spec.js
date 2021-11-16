143937936dc64f2b6806a7e627d1bca4dee0435c
Pechenyice <german.iskhakov@mail.ru>
2021-11-16 23:21:56 +0300

    fixed style with Prettier

--- cypress/integration/0_initial.spec.js
+++ cypress/integration/0_initial.spec.js
@@ -1,17 +1,16 @@
 describe('App loads right', () => {
-  it('show landing content', () => {
+  it('shows landing content', () => {
     cy.visit('http://localhost:3000');
 
-    /*
+    /**
      * check page renders without errors
      */
-    cy.xpath('/html/body/div/section/section[1]/div[1]/h1')
-      .should(
-        "have.text",
-        "Business area for business idea"
-      );
+    cy.xpath('/html/body/div/section/section[1]/div[1]/h1').should(
+      'have.text',
+      'Business area for business idea'
+    );
 
-    /*
+    /**
      * go to 1_auth spec
      */
     cy.xpath('/html/body/div/section/header/div[2]/a[3]').click();
